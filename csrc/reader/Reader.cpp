5eaa9f15fcb78590647573315a31df7da43e4fe5
Fedor Kovalev <kovalevfm@meta.com>
2025-09-29 10:06:30 -0700

    add extract_audio binding
    Summary:
    extract_audio_track doesn't work for some vrs, for example for nebula vrs gaia:1527115228707833 it gives `"Found non-PCM audio block"` and doesn't create audio file
    
    Adding extract_audio binding to python, so all audio streams can be extracted.
    
    How bad is new dependencie from cli? As alternative we can move extract audio function to vrs/utils
    
    Reviewed By: georges-berenger
    
    Differential Revision: D83349298
    
    fbshipit-source-id: 18d9ef0e2a613a02aa086c49acceedad81ad537b

--- csrc/reader/Reader.cpp
+++ csrc/reader/Reader.cpp
@@ -18,6 +18,7 @@
 
 #include <vrs/os/Platform.h>
 #include <vrs/utils/AudioTrackExtractor.h>
+#include <vrs/utils/cli/DataExtraction.h>
 
 #include "AsyncVRSReader.h"
 #include "FilteredFileReader.h"
@@ -30,6 +31,11 @@ namespace py = pybind11;
 string extractAudioTrack(pyvrs::FilteredFileReader& filteredReader, const string& wavFilePath) {
   initVrsBindings();
   return vrs::utils::extractAudioTrack(filteredReader.getFilteredReader(), wavFilePath);
+}
+
+int extractAudio(const string& path, pyvrs::FilteredFileReader& filteredReader) {
+  initVrsBindings();
+  return vrs::utils::extractAudio(path, filteredReader.getFilteredReader());
 }
 
 void pybind_reader(py::module& m) {
@@ -54,5 +60,7 @@ void pybind_reader(py::module& m) {
       "extract_audio_track",
       &extractAudioTrack,
       "Extract audio track from given FilteredFileReader");
+
+  m.def("extract_audio", &extractAudio, "Extract all audio tracks from given FilteredFileReader");
 }
 } // namespace pyvrs
